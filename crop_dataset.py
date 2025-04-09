f36451c7961ca7e9cf9d252c55424673e87ad2be
Hyx <christiansmoises023@gmail.com>
2025-04-09 22:55:27 +0800

    added Trucks,Busses, and other cars for detection

--- crop_dataset.py
+++ crop_dataset.py
@@ -12,7 +12,7 @@ def process_images(input_folder):
             image_path = os.path.join(input_folder, filename)
             image = cv2.imread(image_path)
 
-            # Perform car detection
+            # Perform detection
             results = model(image)
 
             # Initialize variables
@@ -22,11 +22,11 @@ def process_images(input_folder):
             for result in results:
                 for box in result.boxes:
                     cls = int(box.cls[0])
-                    # Check if the detected object is a car (class ID 2 in COCO)
-                    if cls == 2:
+                    # Check if the object is a car, bus, or truck
+                    if cls in [2, 5,6,7]:  # Car, Bus, Truck
                         x1, y1, x2, y2 = map(int, box.xyxy[0])
                         area = (x2 - x1) * (y2 - y1)
-                        # Select the largest detected car
+                        # Select the largest detected vehicle
                         if area > largest_area:
                             largest_area = area
                             largest_box = (x1, y1, x2, y2)
@@ -36,8 +36,8 @@ def process_images(input_folder):
                 cropped_image = image[y1:y2, x1:x2]
                 cv2.imwrite(image_path, cropped_image)
             else:
-                print(f"No car detected in {filename}. Skipping cropping.")
+                print(f"No vehicle detected in {filename}. Skipping cropping.")
 
 if __name__ == "__main__":
-    input_folder = 'scraped_dataset/toyota_yaris cross'
+    input_folder = "scraped_dataset_2/nissan_urvan"
     process_images(input_folder)
